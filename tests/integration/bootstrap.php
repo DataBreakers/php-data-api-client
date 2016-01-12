08553128e57387c9bcd205244bdb288bf870169e
Jan Vesely <janvesely@janvesely.net>
2016-01-12 16:15:42 +0100

    move integration test to integration dir

---
+++ tests/integration/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+$loader = include __DIR__ . '/../../vendor/autoload.php';
+$loader->addPsr4('DataBreakers\\', __DIR__);
+
+// Configure environment
+Tester\Environment::setup();
+date_default_timezone_set('Europe/Prague');
+
+// Create temporary directory
+define('TEMP_DIR', __DIR__ . '/temp/' . getmypid());
+Tester\Helpers::purge(TEMP_DIR);
